80442b7c147eb09c0feb03e2664c348ecfd9f5d3
RednedEpic <redned@retronixgames.x10host.com>
2019-07-29 16:40:38 -0500

    Initial commit

---
+++ src/main/java/org/geysermc/geyserresources/GeyserResources.java
@@ -0,0 +1,51 @@
+package org.geysermc.geyserresources;
+
+import com.google.gson.stream.JsonWriter;
+import net.minecraft.server.v1_14_R1.IRegistry;
+import net.minecraft.server.v1_14_R1.Item;
+import net.minecraft.server.v1_14_R1.MinecraftKey;
+import org.bukkit.plugin.java.JavaPlugin;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Optional;
+
+public class GeyserResources extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        createJSONData();
+    }
+
+    public void createJSONData() {
+        File dataFolder = getDataFolder();
+        if (!dataFolder.exists()) {
+            dataFolder.mkdir();
+        }
+
+        try {
+            File file = new File(getDataFolder(), "java_items.json");
+            if (!file.exists()) {
+                file.createNewFile();
+            }
+
+            JsonWriter jsonWriter = new JsonWriter(new FileWriter(file));
+            jsonWriter.beginObject();
+            for (MinecraftKey key : IRegistry.ITEM.keySet()) {
+                Optional<Item> item = IRegistry.ITEM.getOptional(key);
+                if (item.isPresent()) {
+                    jsonWriter.name(key.getNamespace() + ":" + key.getKey());
+                    jsonWriter.beginObject();
+                    jsonWriter.name("protocol_id").value(Item.getId(item.get()));
+                    jsonWriter.endObject();
+                }
+            }
+
+            jsonWriter.endObject();
+            jsonWriter.close();
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+    }
+}
