482e8e8c833c4f51aad23562a0c9965652620d2f
blazewalker462 <blazewalker462@protonmail.com>
2020-04-17 19:01:53 -0700

    Add noteblock pitch parameter

--- src/main/java/org/geysermc/resources/ResourceGenerator.java
+++ src/main/java/org/geysermc/resources/ResourceGenerator.java
@@ -126,6 +126,9 @@ public class ResourceGenerator {
                 String woolid = trimmedIdentifier.replace("minecraft:", "");
                 woolid = woolid.split("_bed")[0].toUpperCase();
                 object.addProperty("bed_color", DyeColor.valueOf(woolid).getId());
+            } else if (trimmedIdentifier.contains("note_block")) {
+                int notepitch = Integer.parseInt(identifier.substring(identifier.indexOf("note=") + 5, identifier.indexOf(",powered")));
+                object.addProperty("note_pitch", notepitch);
             }
 
             if (blockEntry.getBedrockStates() != null)
