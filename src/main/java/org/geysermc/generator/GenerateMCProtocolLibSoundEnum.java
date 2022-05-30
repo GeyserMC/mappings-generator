e84a5225df7160b0a007bfdb73c0bf3a67836b8a
davchoo <davchoo@users.noreply.github.com>
2022-05-30 01:34:23 -0400

    Apply similar fix for GenerateMCProtocolLibSoundEnum

--- src/main/java/org/geysermc/generator/GenerateMCProtocolLibSoundEnum.java
+++ src/main/java/org/geysermc/generator/GenerateMCProtocolLibSoundEnum.java
@@ -1,6 +1,8 @@
 package org.geysermc.generator;
 
 import net.minecraft.core.Registry;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.sounds.SoundEvent;
 
 import java.util.Locale;
 
@@ -9,11 +11,14 @@ public class GenerateMCProtocolLibSoundEnum {
     public static void main(String[] args) {
         Util.initialize();
 
-        Registry.SOUND_EVENT.keySet().forEach((resourceLocation -> {
+        for (int i = 0; i < Registry.SOUND_EVENT.size(); i++) {
+            SoundEvent soundEvent = Registry.SOUND_EVENT.byId(i);
+            ResourceLocation resourceLocation = Registry.SOUND_EVENT.getKey(soundEvent);
+
             String value = resourceLocation.getPath().replace("minecraft:", "");
             String enumName = value.replace(".", "_")
                     .toUpperCase(Locale.ROOT);
             System.out.println(enumName + "(\"" + value + "\"),");
-        }));
+        }
     }
 }
