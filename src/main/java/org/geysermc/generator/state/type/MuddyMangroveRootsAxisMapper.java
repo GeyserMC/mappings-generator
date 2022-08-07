1798f95fe5426fab932b64154558d75392ac8a5f
Camotoy <20743703+Camotoy@users.noreply.github.com>
2022-08-07 12:08:55 -0400

    Update mappings for 1.19.20

---
+++ src/main/java/org/geysermc/generator/state/type/MuddyMangroveRootsAxisMapper.java
@@ -0,0 +1,14 @@
+package org.geysermc.generator.state.type;
+
+import org.apache.commons.lang3.tuple.Pair;
+import org.geysermc.generator.state.StateMapper;
+import org.geysermc.generator.state.StateRemapper;
+
+@StateRemapper(value = "axis", blockRegex = "^minecraft:muddy_mangrove_roots")
+public class MuddyMangroveRootsAxisMapper extends StateMapper<String> {
+
+    @Override
+    public Pair<String, String> translateState(String fullIdentifier, String value) {
+        return Pair.of("pillar_axis", value);
+    }
+}
