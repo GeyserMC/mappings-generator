cb949b0969c823547270f066aab55f0f167c8a60
Konicai <71294714+Konicai@users.noreply.github.com>
2022-11-23 21:14:24 -0500

    Fix states of piglin heads and hanging wall sign

--- src/main/java/org/geysermc/generator/state/type/WallHeadFacingMapper.java
+++ src/main/java/org/geysermc/generator/state/type/WallHeadFacingMapper.java
@@ -4,18 +4,21 @@ import org.apache.commons.lang3.tuple.Pair;
 import org.geysermc.generator.state.StateMapper;
 import org.geysermc.generator.state.StateRemapper;
 
-@StateRemapper(value = "facing", blockRegex = ".*wall_(hanging_)?sign.?$")
-public class WallSignFacingMapper extends StateMapper<Integer> {
+@StateRemapper(value = "facing", blockRegex = ".*_wall_(head|skull)")
+public class WallHeadFacingMapper extends StateMapper<Integer> {
+
+    public WallHeadFacingMapper() {
+    }
 
     @Override
     public Pair<String, Integer> translateState(String fullIdentifier, String value) {
-        int facing = switch (value) {
+        int direction = switch (value) {
             case "north" -> 2;
             case "south" -> 3;
             case "west" -> 4;
             case "east" -> 5;
             default -> 0;
         };
-        return Pair.of("facing_direction", facing);
+        return Pair.of("facing_direction", direction);
     }
 }
