162e1887759d55dd2a5db9894d29ee2b421381e0
Konicai <71294714+Konicai@users.noreply.github.com>
2023-05-11 16:14:24 -0400

    Remove wood_type state from cherry/bamboo fence

--- src/main/java/org/geysermc/generator/state/type/FenceWoodTypeMapper.java
+++ src/main/java/org/geysermc/generator/state/type/FenceWoodTypeMapper.java
@@ -4,7 +4,7 @@ import org.apache.commons.lang3.tuple.Pair;
 import org.geysermc.generator.state.StateMapper;
 import org.geysermc.generator.state.StateRemapper;
 
-@StateRemapper(value = "waterlogged", blockRegex = {".*_fence$", "^((?!nether).)*$", "^((?!mangrove).)*$", "^((?!crimson).)*$", "^((?!warped).)*$"})
+@StateRemapper(value = "waterlogged", blockRegex = {".*_fence$", "^((?!nether).)*$", "^((?!mangrove).)*$", "^((?!crimson).)*$", "^((?!warped).)*$", "^((?!bamboo).)*$", "^((?!cherry).)*$"})
 public class FenceWoodTypeMapper extends StateMapper<String> {
 
     @Override
